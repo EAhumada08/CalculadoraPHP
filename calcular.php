2f111d4aa2aa6745eb5a111d58bf63e11f75bb81
EAhumada08 <emaahmc08@gmail.com>
2023-10-25 21:04:01 -0600

    añadir archivos

---
+++ calcular.php
@@ -0,0 +1,111 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <?php
+    /*if ($_REQUEST['radio1']=="suma") {
+        $suma=$_REQUEST['valor1']+$_REQUEST['valor2'];
+        echo "La suma es: ".$suma;
+    } elseif($_REQUEST['radio1']=="resta"){
+        $resta=$_REQUEST['valor1']-$_REQUEST['valor2'];
+        echo "La resta es: ".$resta;
+    }*/
+
+    switch ($_REQUEST['radio1']) {
+        case 'suma':
+            $suma=$_REQUEST['valor1']+$_REQUEST['valor2'];
+            echo <<< EOT
+            <div class="mt-3 ms-3 col-3">
+            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+            </symbol>
+            </svg>
+
+            <div class="alert alert-success d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+            <div>
+              La suma es: $suma
+            </div>
+            </div>
+            </div>
+            EOT;
+            break;
+        
+        case 'resta':
+            $resta=$_REQUEST['valor1']-$_REQUEST['valor2'];
+            echo <<< EOT
+            <div class="mt-3 ms-3 col-3">
+            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+            </symbol>
+            </svg>
+
+            <div class="alert alert-success d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+            <div>
+              La resta es: $resta
+            </div>
+            </div>
+            </div>
+            EOT;
+            break;
+        
+        case 'multiplicacion':
+            $multi=$_REQUEST['valor1']*$_REQUEST['valor2'];
+            echo <<< EOT
+            <div class="mt-3 ms-3 col-3">
+            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+            </symbol>
+            </svg>
+
+            <div class="alert alert-success d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+            <div>
+              El producto es: $multi
+            </div>
+            </div>
+            </div>
+            EOT;
+            break;
+
+        case 'division':
+            $division=$_REQUEST['valor1']/$_REQUEST['valor2'];
+            echo <<< EOT
+            <div class="mt-3 ms-3 col-3">
+            <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+            <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+            </symbol>
+            </svg>
+
+            <div class="alert alert-success d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+            <div>
+              El cociente es: $division
+            </div>
+            </div>
+            </div>
+            EOT;
+            break;
+
+        default:
+            
+            break;
+    }
+    ?> 
+
+    <br><br>
+
+    <i class="bi bi-box-arrow-left"></i>
+    <a href="index.html" class="btn btn-danger ms-5">Volver</a>
+</body>
+</html>
